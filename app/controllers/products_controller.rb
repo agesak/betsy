80ab5a62c31c49f5217f3cf5b337bdfe90f4aea9
Sophie Messing <sophie.e.messing@gmail.com>
2020-11-17 11:08:29 -0800

    product scaffolding complete

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -71,6 +71,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    return params.require(:product).permit(:name, :cost, :inventory, :description, :image, :merchant_id, :review_id)
+    return params.require(:product).permit(:name, :cost, :inventory, :description, :image)
   end
 end
