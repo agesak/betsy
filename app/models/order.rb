a6da76482216415dd900b2b7165bd1b2fe10bc7b
Kareha Agesa <kmaagesa@gmail.com>
2020-11-17 22:33:44 -0800

    initial instation, relation, and presence order model tests

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,11 @@
 class Order < ApplicationRecord
   belongs_to :cart
+
+  validates :email, presence: true
+  validates :mailing_address, presence: true
+  validates :name, presence: true
+  validates :cc_number, presence: true
+  validates :cc_expiration, presence: true
+  validates :cc_cvv, presence: true
+  validates :zip, presence: true
 end
