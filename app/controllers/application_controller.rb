8487b1d41e73ac95caf08636d5a17919f4f47e70
Lina Do <l.do5147@gmail.com>
2020-11-17 18:00:45 -0800

    added method to application controller to ensure that every guest or user that visits the site has a cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+  def current_cart
+    if session[:cart_id]
+      # check if there is an existing session for the cart
+      cart = Cart.find_by(id: session[:cart_id])
+      @current_cart = cart
+    else
+      # create a new cart if there is no cart session
+      # store the session cart_id
+      @current_cart = Cart.create
+      session[:cart_id] = @current_cart.id
+    end
+  end
+
 end
