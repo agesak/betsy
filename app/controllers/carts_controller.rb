236ca55e7b6307cb032291e6c2abcb739318070f
Lina Do <l.do5147@gmail.com>
2020-11-17 20:21:33 -0800

    added tests for current cart method

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,4 +3,5 @@ class CartsController < ApplicationController
   def show
     @cart = @current_cart
   end
+
 end
--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -4,4 +4,54 @@ describe CartsController do
   # it "does a thing" do
   #   value(1+1).must_equal 2
   # end
+
+  # describe "show" do
+  #
+  #   it "will get show for current cart" do
+  #
+  #     get root_path
+  #
+  #     current_cart = Cart.create()
+  #     session[:cart_id] = cart.id
+  #
+  #
+  #
+  #   end
+  #
+  # end
+  describe 'current cart' do
+    it "creates a new cart when visiting the site for the first time" do
+
+      expect{
+        get root_path
+      }.must_differ "Cart.count", 1
+
+      cart_id = session[:cart_id]
+      current_cart = Cart.find_by(id: cart_id)
+
+      expect(current_cart).wont_be_nil
+      expect(session[:cart_id]).must_equal current_cart.id
+
+    end
+
+    it "should have the same cart throughout the site " do
+
+      get root_path
+      cart_id = session[:cart_id]
+      cart = Cart.find_by(id: cart_id)
+
+      # wait on other controllers to get another path and finish out writing test
+
+      # expect{
+      #   get user_path(users(:ada).id)
+      # }.wont_change "Cart.count"
+      # current_cart_id = session[:cart_id]
+      # current_cart = Cart.find_by(id: current_cart_id)
+      #
+      # expect(current_cart).wont_be_nil
+      # expect(current_cart.id).must_equal cart.id
+
+    end
+  end
+
 end
