378a5467304971035b77fb0ed11359eaedfd48cb
Kareha Agesa <kmaagesa@gmail.com>
2020-11-18 17:39:24 -0800

    order/cartitems relationship NO TESTS

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :cart
+  has_many :cartitems, through: :cart
 
-  validates :email, :mailing_address, :name, :cc_number, :cc_expiration, :cc_cvv, :zip, presence: true
+  validates :email, :mailing_address, :name, :cc_number, :cc_expiration, :cc_cvv, :zip, :cartitems, presence: true
 
 end
