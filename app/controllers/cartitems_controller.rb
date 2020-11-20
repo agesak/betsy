d60459d31ea9fedf108364646cd77202bb877382
Lina Do <l.do5147@gmail.com>
2020-11-20 12:30:45 -0800

    removed create action from cart items

--- app/controllers/cartitems_controller.rb
+++ app/controllers/cartitems_controller.rb
@@ -2,36 +2,6 @@ class CartitemsController < ApplicationController
 
   before_action :find_cartitem, except: [:create]
 
-  # def create
-  #   # product nested route
-  #   product_id = params[:product_id]
-  #   product = Product.find_by(id: product_id)
-  #   current_cart = @current_cart
-  #
-  #   # check if product is already in the cart
-  #   if current_cart.products.include?(product)
-  #
-  #     # find the cart item
-  #     @cart_item = current_cart.cartitems.find_by(product_id: product_id)
-  #     # check if there is enough inventory
-  #     if @cart_item.qty < product.inventory
-  #       @cart_item.qty += 1
-  #     else
-  #       # not enough inventory
-  #       redirect_back fallback_location: root_path
-  #       flash[:error] = "Sorry, not enough inventory"
-  #       return
-  #     end
-  #   else
-  #     # create a new cart item if it doesn't exist
-  #     @cart_item = Cartitem.new(cart_id: current_cart.id, product_id: product_id, qty: 1, cost: product.cost )
-  #   end
-  #
-  #   # save the cart item and redirect back to the product show page
-  #   @cart_item.save
-  #   redirect_back fallback_location: root_path
-  # end
-
   def add_qty
     # find the product
     product_inventory = @cart_item.product.inventory
