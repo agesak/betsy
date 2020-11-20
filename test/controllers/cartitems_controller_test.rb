696b458f6623237e49d7d14b3d9574e9a25f1c7b
Lina Do <l.do5147@gmail.com>
2020-11-20 14:47:33 -0800

    removed create action tests for cartitem controller

--- test/controllers/cartitems_controller_test.rb
+++ test/controllers/cartitems_controller_test.rb
@@ -5,41 +5,6 @@ describe CartitemsController do
   #   value(1+1).must_equal 2
   # end
 
-  describe "create" do
-    it "can create a cart item" do
-      # start a new cart
-      get root_path
-      cart = Cart.find_by(id: session[:cart_id])
-
-      expect{
-        post product_cartitems_path(products(:product0).id)
-      }.must_differ "cart.cartitems.count", 1
-
-      must_respond_with :redirect
-    end
-
-    it "will not add to the cart if there is not enough inventory" do
-      # start a new cart
-      get root_path
-      cart = Cart.find_by(id: session[:cart_id])
-
-      # add the product to the cart, product only has one in inventory
-      post product_cartitems_path(products(:product1).id)
-
-      cart_item = cart.cartitems.find_by(product: products(:product1))
-
-      # add the same product again
-      expect{
-        post product_cartitems_path(products(:product1).id)
-      }.wont_change "cart_item.qty"
-
-      expect(flash[:error]).wont_be_nil
-      must_respond_with :redirect
-
-    end
-
-  end
-
   describe "add_qty" do
     it "can increase the quantity of the cart item by 1" do
 
