08483ddc136bdb927e52b9dd7d508486ce95c8ba
Kareha Agesa <kmaagesa@gmail.com>
2020-11-20 16:27:08 -0800

    cart purchase fails when info isnt filled out (and tests)

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,7 @@
 class Cart < ApplicationRecord
   has_many :cartitems
   has_many :products, through: :cartitems
+
+  validates_presence_of :email, :mailing_address, :name, :cc_number, :cc_expiration, :cc_cvv, :zip,  :if => lambda {self.status != "pending"}
+
 end
