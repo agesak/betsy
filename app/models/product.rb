7f6a592b519032fdf88ffa9446bb5bdb5fb21a8c
Sophie Messing <sophie.e.messing@gmail.com>
2020-11-22 21:47:09 -0800

    Playing with image rendering

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,8 @@ class Product < ApplicationRecord
   has_and_belongs_to_many :categories
   belongs_to :user
 
-  validates :name, :description, :image, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
   validates :inventory, :cost, presence: true, numericality: { greater_than: 0 }
   validates :category_ids, presence: true
 
