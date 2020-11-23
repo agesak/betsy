d9a10449a03faa57e4683d043fc4d4a0c37b3889
Lina Do <l.do5147@gmail.com>
2020-11-23 11:44:22 -0800

    added relationships between product and review

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   has_many :cartitems
   has_and_belongs_to_many :categories
   belongs_to :user
+  has_many :reviews
 
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
