2a6beabe9e8acb9d6bacab8285023857044504d1
Lina Do <l.do5147@gmail.com>
2020-11-23 15:40:10 -0800

    added a custom message to rating validator

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,6 @@ class Review < ApplicationRecord
   belongs_to :product
 
   validates :name, presence: true
-  validates :rating, presence: true
+  validates_presence_of :rating, message: "must give a rating"
+
 end
