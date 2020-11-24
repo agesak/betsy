e1608aaadfb33dc349a2c2fd01a05655318e8a68
Lina Do <l.do5147@gmail.com>
2020-11-24 09:00:47 -0800

    changes validation messages

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
   belongs_to :product
 
   validates :name, presence: true
-  validates_presence_of :rating, message: "must give a rating"
-  validates :rating, :inclusion => 1..5
+  validates :rating, presence: true
+  validates :rating, inclusion: { in: 1..5, message: "rating needs to be between 1-5" }
 
 end
