f5a2772eeeab0f3987fca6ad81568747ef3beea2
Lina Do <l.do5147@gmail.com>
2020-11-25 12:29:17 -0800

    added more tests to make sure user cannot review their own product and tweaked other tests

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,12 +3,11 @@ class ReviewsController < ApplicationController
   def create
     product = Product.find_by(id: params[:product_id])
     if product.user == @current_user
-      flash[:error] = 'You cannot review your own product'
+      flash[:error] = 'You cannot review your own product!'
       redirect_to product_path(product.id)
       return
     else
       @review = Review.new(review_params)
-
       @review.product = product
       if @review.save
         flash[:success] = 'Thank you for your review!'
--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -27,8 +27,9 @@ describe ReviewsController do
 
     it "creates a review with valid information while logged in" do
 
-      perform_login
+      perform_login(user = users(:valentine))
 
+      #user writes a review for a product created by another merchant
       product = products(:product0)
       new_review = {
           review: {
@@ -44,6 +45,31 @@ describe ReviewsController do
       }.must_change "Review.count", 1
 
       expect(flash[:success]).must_equal "Thank you for your review!"
+      must_respond_with :redirect
+      must_redirect_to product_path(product)
+
+    end
+
+    it "a user cannot review own product" do
+
+      perform_login(user = users(:ada))
+
+      # user tries reviewing own product
+      product = products(:product0)
+      new_review = {
+          review: {
+              name: "Kristal Calimari",
+              rating: 5,
+              description: "Best product ever"
+
+          }
+      }
+
+      expect {
+        post product_reviews_path(product.id), params: new_review
+      }.wont_change "Review.count"
+
+      expect(flash[:error]).must_equal 'You cannot review your own product!'
       must_respond_with :redirect
       must_redirect_to product_path(product)
 
