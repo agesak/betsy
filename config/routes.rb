2a45d81ce4016dbde94c892619f0b1116d02e61d
r-spiel <richelle.mehlhaff@gmail.com>
2020-11-25 16:10:48 -0800

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :products do
     resources :reviews, only:[:create]
   end
-  # moved adding item to cart from cartitem controller to product
   post "/products/:id/add_to_cart,", to: "products#add_to_cart", as: "add_to_cart"
 
 
@@ -20,13 +19,9 @@ Rails.application.routes.draw do
   # Omniauth Github callback route
   get "/auth/:provider/callback", to: "users#create", as: "omniauth_callback"
 
-  # User routes
   delete "/logout", to: "users#destroy", as: "logout"
   get "/users/current", to: "users#current", as: "current_user"
   get "/users/current/fulfillment", to: "users#fulfillment", as: "current_user_fulfillment"
-
-  # Nested Route (not tested) - to link to separate page for current user products
-  #get "/user/:user_id/products", to: "products#index", as: "current_user_products"
 
   get 'carts/:id', to: "carts#show", as: "cart"
   get 'carts/:id/purchase', to: "carts#purchase_form", as: "purchase_form"
